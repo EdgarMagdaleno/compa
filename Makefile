729fccb9bd14d91b75e5073ee89d58dea6b6dc58
Edgar Magdaleno <edgarmv97@gmail.com>
2018-10-17 20:34:11 -0700

    Update

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc -Wall -Wextra *.c -o compa -lm
+	gcc *.c -o compa -lm
--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include <math.h>
 
 #define MAX_WORD_SIZE 255
 
-FILE *source, out;
-int line = 1, col = 0;
+FILE *source, *out;
+int line = 1, col = 0, ch = ' ';
 
 void error_exit(const char *msg) {
 	printf("[ERROR] %s\n", msg);
@@ -16,7 +17,8 @@ void error_exit(const char *msg) {
 typedef enum {
 	tk_char, tk_dobl, tk_else, tk_if,   tk_int,  tk_str,  tk_whle, tk_eos,
 	tk_add,  tk_sub,  tk_mul,  tk_div,  tk_mod,  tk_eq,   tk_neq,  tk_grt,
-	tk_grte, tk_les,  tk_lese
+	tk_grte, tk_les,  tk_lese, tk_id, 	tk_lint, tk_ldbl, tk_asg,  tk_lpar,
+	tk_rpar, tk_lbrc, tk_rbrc, tk_coma, tk_lstr,  tk_lchr
 } token_type;
 
 struct token {
@@ -42,94 +44,133 @@ struct {
 	{"int",		tk_int},
 	{"string",	tk_str},
 	{"while",	tk_whle},
-};
+}, *kw;
 
-struct token *get_number() {
-	double n;
-	struct token *tok;
-	fseek(source, -1, SEEK_CUR);
-	
-	long offset = ftell(source);
-	if (fscanf(source, "%lf", &n) != 1)
-		return NULL;
-	long diff = ftell(source) - offset;
+struct token *new_token(token_type type) {
+	struct token *tok = malloc(sizeof(struct token));
+	tok->type = type;
 
-	tok = malloc(sizeof(struct token));
-	if (ceil(n) == n) {
-		tok->type = tk_int;
-		tok->i = (int) n;
-		goto ret;
-	}
-
-	tok->type = tk_dobl;
-	tok->d = n;
-ret:
-	tok->line = line;
-	tok->col = col;
-	col += diff;
 	return tok;
 }
 
-int getc_source() {
-	int c = getc(source);
+struct token *number() {
+	double n;
+	struct token *tok = NULL;
+	ungetc(ch, source);
+	
+	if (fscanf(source, "%lf", &n) != 1)
+		return NULL;
+
+	tok = malloc(sizeof(struct token));
+	if (ceil(n) == n) {
+		tok->type = tk_lint;
+		tok->i = (int) n;
+		return tok;
+	}
+
+	tok->type = tk_ldbl;
+	tok->d = n;
+	return tok;
+}
+
+void getc_source() {
+	ch = getc(source);
 	col++;
 
-	if (c == '\n') {
+	if (ch == '\n') {
 		line++;
 		col = 0;
 	}
+}
+
+int comp(const void *a, const void *b) {
+	return strcmp((char *) a, *(char **) b);
+}
+
+struct token *follow(token_type normal, token_type follow, char next) {
+	struct token *tok = malloc(sizeof(struct token));
 	
-	return c;
+	getc_source();
+	tok->type = ch == next ? follow : normal;
+	return tok;
+}
+
+struct token *ident_or_kw() {
+	struct token *tok = malloc(sizeof(struct token));
+	ungetc(ch, source);
+
+	char buf[MAX_WORD_SIZE];
+	fscanf(source, "%255[a-zA-Z0-9_]", buf);
+	tok->type = !(kw = bsearch(buf, keywords, sizeof(keywords) / sizeof(keywords[0]),
+				  sizeof(keywords[0]), comp)) ? tk_id : kw->type;
+
+	if (tok->type == tk_id) {
+		tok->s = malloc(strlen(buf));
+		strcpy(tok->s, buf);
+	}
+
+	return tok;
+}
+
+struct token *string_lit() {
+	char buf[MAX_WORD_SIZE];
+	fscanf("%255[^\"]\"");
+}
+
+struct token *char_lit() {
+	return NULL;
 }
 
 struct token *get_token() {
-	switch(getc_source()) {
-		case 'a' ... 'z':
-		case 'A' ... 'Z':
-				
-			break;
-		case '0' ... '9':
-			return get_number();
-			break;
+	struct token *tok = NULL;
+	while (isspace(ch))
+		getc_source();
 
-		case EOF: return NULL; break;
-		default: printf("Unrecognized character\n"); break;
-/*
-        case '}':  next_ch(); return (tok_s){tk_Rbrace, err_line, err_col, {0}};
-        case '(':  next_ch(); return (tok_s){tk_Lparen, err_line, err_col, {0}};
-        case ')':  next_ch(); return (tok_s){tk_Rparen, err_line, err_col, {0}};
-        case '+':  next_ch(); return (tok_s){tk_Add, err_line, err_col, {0}};
-        case '-':  next_ch(); return (tok_s){tk_Sub, err_line, err_col, {0}};
-        case '*':  next_ch(); return (tok_s){tk_Mul, err_line, err_col, {0}};
-        case '%':  next_ch(); return (tok_s){tk_Mod, err_line, err_col, {0}};
-        case ';':  next_ch(); return (tok_s){tk_Semi, err_line, err_col, {0}};
-        case ',':  next_ch(); return (tok_s){tk_Comma,err_line, err_col, {0}};
-        case '/':  next_ch(); return div_or_cmt(err_line, err_col);
-        case '\'': next_ch(); return char_lit(the_ch, err_line, err_col);
-        case '<':  next_ch(); return follow('=', tk_Leq, tk_Lss,    err_line, err_col);
-        case '>':  next_ch(); return follow('=', tk_Geq, tk_Gtr,    err_line, err_col);
-        case '=':  next_ch(); return follow('=', tk_Eq,  tk_Assign, err_line, err_col);
-*/
+	int tok_line = line;
+	int tok_col = col;
+	switch(ch) {
+		case '=': tok = follow(tk_asg, tk_eq, '='); break;
+		case '>': tok = follow(tk_grt, tk_grte, '='); break;
+		case '<': tok = follow(tk_les, tk_lese, '='); break;
+		case '!': tok = new_token(tk_neq); break;
+		case '+': tok = new_token(tk_add); break;
+		case '-': tok = new_token(tk_sub); break;
+		case '*': tok = new_token(tk_mul); break;
+		case '/': tok = new_token(tk_div); break;
+		case '%': tok = new_token(tk_mod); break;
+		case '{': tok = new_token(tk_lbrc); break;
+		case '}': tok = new_token(tk_rbrc); break;
+		case '(': tok = new_token(tk_lpar); break;
+		case ')': tok = new_token(tk_rpar); break;
+		case ';': tok = new_token(tk_eos); break;
+		case '"': tok = string_lit(); break;
+		case '\'': tok = char_lit(); break;
+		case 'a' ... 'z': case 'A' ... 'Z': tok = ident_or_kw(); break;
+		case '0' ... '9': tok = number(); break;
+		default: printf("Unrecognized character: \'%c\': %i\n", ch, ch); case EOF: break;
 	}
 
-	return NULL;
+	if (tok) {
+		tok->line = tok_line;
+		tok->col = tok_col;
+		getc_source();
+	}
+
+	return tok;
 }
 
 void print_token(struct token *tok) {
 	printf("%i %i %.8s", tok->line, tok->col,
 		&"tk_char tk_dobl tk_else tk_if   tk_int  tk_str  tk_whle tk_eos  "
 		 "tk_add  tk_sub  tk_mul  tk_div  tk_mod  tk_eq   tk_neq  tk_grt  "
-		 "tk_grte tk_les  tk_lese "[tok->type * 8]
+		 "tk_grte tk_les  tk_lese tk_id   tk_cint tk_cdbl tk_asg  tk_lpar "
+		 "tk_rpar tk_lbrc tk_rbrc tk_coma tk_lstr tk_lchr "[tok->type * 8]
 	);
 	
 	switch (tok->type) {
-		case tk_int:
-			printf("%i" , tok->i);
-			break;
-
-		case tk_dobl:
-			printf("%lf", tok->d);
-			break;
+		case tk_id: printf("%s", tok->s); break;
+		case tk_lint: printf("%i" , tok->i); break;
+		case tk_ldbl: printf("%lf", tok->d); break;
 		default: break;
 	}
 
